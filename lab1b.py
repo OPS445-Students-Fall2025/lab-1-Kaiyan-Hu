382ba41655a0feba75905b011090f06a791be9d3
Kaiyan Hu <khu27@myseneca.ca>
2025-09-10 18:44:00 -0400

    Added lab1b

---
+++ lab1b.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''
+Description: This program will output a name and ask about the age.
+'''
+
+# make a new object 'name' containing a value: Thomas
+name = 'Isaac'
+
+#  print out the string and conbine it with name value
+print('How old are you ' + name + '?')
+
+
+
+
