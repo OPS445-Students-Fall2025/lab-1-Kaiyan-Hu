6c022b4bcf86d6a9ef68fe424bc8be24a6bd7761
Kaiyan Hu <khu27@myseneca.ca>
2025-09-10 19:32:41 -0400

    Added lab1c

---
+++ lab1c.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+'''
+Desciption: Demostrate string(name) and integer(age) Objects by printing a sentence
+'''
+
+# assign a string value to 'name'
+name = 'Isaac'
+
+# assign an integer value to 'age'
+age = 72
+
+
+# Combine the name and age into one output string
+print(name + ' is '+ str(age) + ' years old!')
+
