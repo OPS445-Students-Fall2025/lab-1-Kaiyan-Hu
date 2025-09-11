810090ccc5d2bc79f16493896ec1b41e0b49648e
Kaiyan Hu <khu27@myseneca.ca>
2025-09-10 21:12:45 -0400

    Added lab1d

---
+++ lab1d.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+'''
+Desription:  Explore how Python performs math operations (addition, subtraction, multiplication,
+division, exponents)
+'''
+
+# assign integer values to 'x','y','z'
+x = 10
+y = 2
+z = 5
+
+
+# assign the result of math operation to 'result'
+result = x + y * z
+
+
+# output the math operation and result in a string
+print(str(x) + ' + ' + str(y) + ' * ' + str(z) + ' = ' + str(result))
